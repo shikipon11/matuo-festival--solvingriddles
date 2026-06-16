03174d717155493ae69945ee82b29066233a3b31
shikipon11 <shikipon11@gmail.com>
2026-06-16 19:41:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,40 @@
-const correctPassword = "matsuo69"; // ←ここを謎解きの答えに変更
+const correctPassword = "matsuo69";
 
 function checkPassword() {
 
-    const input =
-        document.getElementById("password").value;
+    const input = document.getElementById("password").value;
+    const message = document.getElementById("message");
+    const button = document.getElementById("actionButton");
 
-    const message =
-        document.getElementById("message");
-if(input === correctPassword){
+    if(input === correctPassword){
 
-    message.style.color = "#4ade80";
-    message.innerHTML = "アクセス認証中...";
+        message.style.color = "#4ade80";
+        message.innerHTML = "アクセス認証中...";
 
-    setTimeout(() => {
+        setTimeout(() => {
 
-        message.innerHTML = "ロック解除成功";
+            message.innerHTML = "ロック解除成功";
 
-        // ロック解除ボタンを非表示
-        document.querySelector("button[onclick='checkPassword()']")
-            .style.display = "none";
+            // ボタンの文字を変更
+            button.textContent = "次に進む";
 
-        // 次へ進むボタンを表示
-        document.getElementById("nextButton")
-            .style.display = "block";
+            // ボタンを押したときの処理を変更
+            button.onclick = goNext;
 
-    },1000);
-}
-else{
+        }, 1000);
+
+    } else {
 
         message.style.color = "#f87171";
         message.innerHTML = "パスワードが違います";
 
         setTimeout(() => {
-
-        // メッセージを消す
             message.innerHTML = "";
-
-        // 入力欄を空にする
             document.getElementById("password").value = "";
-
-        }, 1000); // 2秒後
-
-    }    
+        }, 1000);
+    }
 }
+
 function goNext(){
     window.location.href = "https://sites.google.com/view/matsuofes69/%E3%83%9B%E3%83%BC%E3%83%A0?authuser=2";
 }
